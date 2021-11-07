4695f74cd9ff187f8ab4f328c8fc4a8034f0b228
sakibcse570 <17201122@uap-bd.edu>
2021-11-08 02:56:32 +0600

    Functions added: showCart(), getProductInfo() and addToCart(), functionality added: Add Product and total price update

--- app.js
+++ app.js
@@ -6,12 +6,56 @@ const clearBtn = document.querySelector('#clear');
 const totalM = document.getElementById('total');
 let total = 0;
 
-for(var i = 0; i< products.length; i++) {
-    products[i].addEventListener('click',buyProducts);
+for (var i = 0; i < products.length; i++) {
+    products[i].addEventListener('click', buyProducts);
 }
+
+function showCart() {
+    let products = getProducts();
+    products.forEach(product => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <th><img style="height: 50px; width: 100%; display: block;" src="${product.img}"></th>
+            <th>${product.title}</th>
+            <th>${product.price}</th>
+            <th><a href="#" class="badge badge-danger delete" data-id="${product.id}">X</a></th>
+        `;
+        tbody.appendChild(row);
+        total = total + parseInt(product.price);
+        totalM.innerHTML = `${total} ৳`;
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded',showCart);
 
 function buyProducts(e) {
     e.preventDefault();
-    const product = e.target.parentElement; 
-    console.log(e.target.parentElement);
+    const product = e.target.parentElement;
+    //console.log(e.target.parentElement);
+    getProductInfo(product);
 }
+
+function getProductInfo(product) {
+    const productInfo = {
+        img : product.querySelector('img').src,
+        title : product.querySelector('h3').textContent,
+        price : product.querySelector('#price').textContent,
+        id : product.querySelector('a').getAttribute('data-id')
+    };
+    addToCart(productInfo);
+}
+
+function addToCart(productInfo) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <th><img style="height: 50px; width: 100%; display: block;" src="${productInfo.img}"></th>
+        <th>${productInfo.title}</th>
+        <th>${productInfo.price}</th>
+        <th><a href="#" class="badge badge-danger delete" data-id="${productInfo.id}">X</a></th>
+        `;
+    tbody.appendChild(row);
+    total = total + parseInt(productInfo.price);
+    totalM.innerHTML = `${total} ৳`;
+}
+
