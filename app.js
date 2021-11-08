7bba563173ecf819f6123b0b62de889e082af7d1
sakibcse570 <17201122@uap-bd.edu>
2021-11-08 14:06:11 +0600

    remove product and local storage functionality added

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const clearBtn = document.querySelector('#clear');
 const totalM = document.getElementById('total');
 let total = 0;
 
+//addEventListener
+
 for (var i = 0; i < products.length; i++) {
     products[i].addEventListener('click', buyProducts);
 }
+
+document.addEventListener('DOMContentLoaded', showCart);
+tbody.addEventListener('click', removeProduct);
+clearBtn.addEventListener('click', clearCart);
+
+//functions
 
 function showCart() {
     let products = getProducts();
@@ -23,25 +31,21 @@ function showCart() {
         tbody.appendChild(row);
         total = total + parseInt(product.price);
         totalM.innerHTML = `${total} ৳`;
-
     });
 }
-
-document.addEventListener('DOMContentLoaded',showCart);
 
 function buyProducts(e) {
     e.preventDefault();
     const product = e.target.parentElement;
-    //console.log(e.target.parentElement);
     getProductInfo(product);
 }
 
 function getProductInfo(product) {
     const productInfo = {
-        img : product.querySelector('img').src,
-        title : product.querySelector('h3').textContent,
-        price : product.querySelector('#price').textContent,
-        id : product.querySelector('a').getAttribute('data-id')
+        img: product.querySelector('img').src,
+        title: product.querySelector('h3').textContent,
+        price: product.querySelector('#price').textContent,
+        id: product.querySelector('a').getAttribute('data-id')
     };
     addToCart(productInfo);
 }
@@ -57,5 +61,51 @@ function addToCart(productInfo) {
     tbody.appendChild(row);
     total = total + parseInt(productInfo.price);
     totalM.innerHTML = `${total} ৳`;
+    //save info to local storage
+    saveProduct(productInfo);
 }
 
+function saveProduct(productInfo) {
+    let products = getProducts();
+    products.push(productInfo);
+    localStorage.setItem('products', JSON.stringify(products));
+}
+
+function getProducts() {
+    let products;
+    if (localStorage.getItem('products') === null) { //get all products from local storage
+        products = [];
+    } else {
+        products = JSON.parse(localStorage.getItem('products'));
+    }
+    return products;
+}
+
+
+function removeProduct(e) {
+    let prodcutsList, product, productId;
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.parentElement.remove();
+        product = e.target.parentElement.parentElement;
+        productId = product.querySelector('a').getAttribute('data-id');
+    }
+
+    prodcutsList = getProducts();
+    prodcutsList.forEach((product, index) => {
+        if (product.id === productId) {     //remove product from local storage
+            prodcutsList.splice(index, 1);
+            total = total - parseInt(product.price);
+            totalM.innerHTML = `${total} ৳`;
+        }
+    });
+    localStorage.setItem('products', JSON.stringify(prodcutsList));
+}
+
+
+function clearCart() {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    localStorage.clear();
+    totalM.innerHTML = '';
+}
